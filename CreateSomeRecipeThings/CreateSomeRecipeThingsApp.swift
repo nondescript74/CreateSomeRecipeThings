c9b2661b6786a82eba97bc101e3b8cb5b7863b1c
Z <headydiscy@gmail.com>
2024-09-07 08:38:09 -0600

    Initial Commit

---
+++ CreateSomeRecipeThings/CreateSomeRecipeThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateSomeRecipeThingsApp.swift
+//  CreateSomeRecipeThings
+//
+//  Created by Zahirudeen Premji on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CreateSomeRecipeThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CreateSomeRecipeThingsTests/CreateSomeRecipeThingsTests.swift
@@ -0,0 +1,16 @@
+//
+//  CreateSomeRecipeThingsTests.swift
+//  CreateSomeRecipeThingsTests
+//
+//  Created by Zahirudeen Premji on 9/7/24.
+//
+
+import Testing
+
+struct CreateSomeRecipeThingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
