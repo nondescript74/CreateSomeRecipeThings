732844b5dd33c6a13da1318f3512b2436300b9d0
Z <headydiscy@gmail.com>
2024-09-23 15:03:50 -0600

    with views being changed as functions added

---
+++ CreateSomeRecipeThings/Observables/UserRecipes.swift
@@ -0,0 +1,8 @@
+//
+//  UserRecipes.swift
+//  CreateSomeRecipeThings
+//
+//  Created by Zahirudeen Premji on 9/20/24.
+//
+
+import Foundation
---
+++ CreateSomeRecipeThings/Views/SelectCurrentRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectCurrentRecipeView.swift
+//  CreateSomeRecipeThings
+//
+//  Created by Zahirudeen Premji on 9/22/24.
+//
+
+import SwiftUI
+
+struct SelectCurrentRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectCurrentRecipeView()
+}
